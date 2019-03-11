1d9f09a871b136af752b73dcbee53432088377ce
Danny Tseng <dtfocus1025@gmail.com>
2019-03-11 04:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var animal = 'dog'
 
 function myAnimal() {
+  if (animal === 'cat') {
+    animal = 'dog';
+  }
   return animal
 }
 
@@ -8,6 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = 'cat';
   return animal
 }
 
